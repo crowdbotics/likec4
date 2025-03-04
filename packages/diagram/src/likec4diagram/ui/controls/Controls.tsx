36dee632757f2e5ba96f660648c557da0c9a500d
Brenley Dueck <brenley@crowdbotics.com>
2025-03-04 11:30:47 -0600

    ASBLY-773: Add zoom in/out

--- packages/diagram/src/likec4diagram/ui/controls/Controls.tsx
+++ packages/diagram/src/likec4diagram/ui/controls/Controls.tsx
@@ -1,6 +1,7 @@
 import { ActionIconGroup, Badge, Loader, Stack } from '@mantine/core'
 import { openSpotlight } from '@mantine/spotlight'
-import { IconFileSymlink, IconFocusCentered, IconSearch } from '@tabler/icons-react'
+import { IconFileSymlink, IconFocusCentered, IconSearch, IconZoomIn, IconZoomOut } from '@tabler/icons-react'
+import { useReactFlow } from '@xyflow/react'
 import { clsx } from 'clsx'
 import { memo } from 'react'
 import { IfNotEnabled, useDiagramEventHandlers, useEnabledFeatures } from '../../../context'
@@ -17,45 +18,36 @@ import { NavigationButtons } from './NavigationButtons'
 import * as css from './styles.css'
 
 export const Controls = memo(() => {
-  const { viewId, hasLayoutDrift, viewportChanged } = useDiagramContext(c => ({
+  const { viewId, hasLayoutDrift, viewportChanged } = useDiagramContext((c) => ({
     viewId: c.view.id,
     hasLayoutDrift: c.view.hasLayoutDrift ?? false,
     viewportChanged: c.viewportChangedManually,
   }))
   const diagram = useDiagram()
-  const {
-    onOpenSource,
-  } = useDiagramEventHandlers()
-  const {
-    enableControls,
-    enableSearch,
-    enableNavigationButtons,
-    enableReadOnly,
-    enableEdgeEditing,
-    enableVscode,
-  } = useEnabledFeatures()
+  const { onOpenSource } = useDiagramEventHandlers()
+  const { enableControls, enableSearch, enableNavigationButtons, enableReadOnly, enableEdgeEditing, enableVscode } =
+    useEnabledFeatures()
   const notReadOnly = !enableReadOnly
   const portalProps = useMantinePortalProps()
+  const { zoomIn, zoomOut } = useReactFlow()
   return (
     <>
       <Stack
-        className={clsx(
-          'react-flow__panel',
-          css.panel,
-          'likec4-top-left-panel',
-        )}
+        className={clsx('react-flow__panel', css.panel, 'likec4-top-left-panel')}
         align="flex-start"
         onClick={stopPropagation}
-        gap={'xs'}>
+        gap={'xs'}
+      >
         {enableNavigationButtons && <NavigationButtons />}
         <ActionIconGroup className={css.actionIconGroup} orientation="vertical">
           {enableControls && enableSearch && (
             <Tooltip label="Search (Ctrl+F or ⌘+F)" {...portalProps}>
               <ActionIcon
-                onClick={e => {
+                onClick={(e) => {
                   e.stopPropagation()
                   openSpotlight()
-                }}>
+                }}
+              >
                 <IconSearch />
               </ActionIcon>
             </Tooltip>
@@ -63,10 +55,11 @@ export const Controls = memo(() => {
           {enableVscode && (
             <Tooltip label="Open source" {...portalProps}>
               <ActionIcon
-                onClick={e => {
+                onClick={(e) => {
                   e.stopPropagation()
                   onOpenSource?.({ view: viewId })
-                }}>
+                }}
+              >
                 <IconFileSymlink stroke={1.5} />
               </ActionIcon>
             </Tooltip>
@@ -79,25 +72,46 @@ export const Controls = memo(() => {
             </>
           )}
           {enableControls && (
-            <Tooltip label={viewportChanged ? 'Center camera' : 'Camera is centered'} {...portalProps}>
+            <Tooltip label={'Recenter'} {...portalProps}>
               <ActionIcon
-                onClick={e => {
+                onClick={(e) => {
                   e.stopPropagation()
                   diagram.fitDiagram()
-                }}>
+                }}
+              >
                 <IconFocusCentered />
               </ActionIcon>
             </Tooltip>
           )}
         </ActionIconGroup>
         {notReadOnly && <SyncLayoutBadge />}
+        <Tooltip label="Zoom In" {...portalProps}>
+          <ActionIcon
+            onClick={(e) => {
+              e.stopPropagation()
+              zoomIn()
+            }}
+          >
+            <IconZoomIn />
+          </ActionIcon>
+        </Tooltip>
+        <Tooltip label="Zoom Out" {...portalProps}>
+          <ActionIcon
+            onClick={(e) => {
+              e.stopPropagation()
+              zoomOut()
+            }}
+          >
+            <IconZoomOut />
+          </ActionIcon>
+        </Tooltip>
       </Stack>
     </>
   )
 })
 
 const SyncLayoutBadge = () => {
-  const isPending = useDiagramSyncLayoutState(s => s.hasTag('pending'))
+  const isPending = useDiagramSyncLayoutState((s) => s.hasTag('pending'))
   if (!isPending) return null
   return (
     <Badge color="pink" radius={'xs'} size="xs" variant="light" leftSection={<Loader color={'orange'} size={8} />}>
